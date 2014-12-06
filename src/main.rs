5544c0ef05006a6ab4291a7a912644174a73e81d
Scott Talbot <s@chikachow.org>
2014-12-07 09:13:18 +1100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+#![feature(if_let)]
+#![feature(slicing_syntax)]
+
+extern crate collections;
 extern crate serialize;
 extern crate git2;
 
-use std::fmt::{Show, Formatter};
 use serialize::hex::{ToHex};
 
 
@@ -29,27 +32,42 @@ fn main() {
     }
   };
 
-  let submodules: Vec<SubmoduleUpdate> = submodules.iter().filter_map(|submodule| {
+  let submodule_updates: Vec<SubmoduleUpdate> = submodules.iter().filter_map(|submodule| {
     SubmoduleUpdate::from_submodule(submodule)
   }).collect();
 
-  for submodule in submodules.iter() {
-    println!("a: {}", submodule);
+  let mut title = String::new();
+  for submodule in submodule_updates.iter() {
+    let title_component = submodule.get_title();
+    if title.len() > 0 {
+      title.push_str(", ");
+    }
+    title.push_str(&**title_component);
+  }
+
+  println!("Update {}", title);
+
+  for submodule in submodule_updates.iter() {
+    match submodule.get_message() {
+      Some(message) => println!("\n{}", message),
+      None => (),
+    }
   }
 }
 
 
+#[deriving(Show)]
 struct SubmoduleUpdate<'a> {
-  submodule: &'a git2::Submodule<'a>,
-  title: String,
+  name: collections::string::String,
+  title: collections::string::String,
+  message: Option<collections::string::String>,
 }
 
 impl<'a> SubmoduleUpdate<'a> {
-  // pub fn new(submodule: &'a git2::Submodule) -> SubmoduleUpdate<'a> {
-  //   SubmoduleUpdate{ submodule: submodule }
-  // }
-
   pub fn from_submodule(submodule: &'a git2::Submodule) -> Option<SubmoduleUpdate<'a>> {
+    let path = submodule.path();
+    let name = String::from_str(path.as_str().or(submodule.name()).unwrap_or("???"));
+
     let current_id = match submodule.head_id() {
       Some(id) => id,
       None => return None,
@@ -59,55 +77,101 @@ impl<'a> SubmoduleUpdate<'a> {
       None => return None,
     };
 
-    // if current_id == new_id {
-    //   return None;
-    // }
-
-    let path = submodule.path();
-    let name = path.as_str().or(submodule.name());
-
-    let change = format!("{}..{}", current_id.as_bytes()[0..4].to_hex(), new_id.as_bytes()[0..4].to_hex());
-
-    let title = format!("{} ({})", name, change);
-
-    let r = match git2::Repository::open(&path) {
-      Ok(r) => r,
-      Err(_) => return None,
-    };
-
-    let added_walk = match r.new_revwalk() {
-      Ok(rw) => rw,
-      Err(_) => return None,
-    };
-    added_walk.set_sorting(git2::SORT_TOPOLOGICAL);
-    added_walk.hide(&current_id);
-    added_walk.push(&new_id);
-
-    for oid in added_walk.oid_iter() {
-      println!("+{}", oid);
+    if current_id == new_id {
+      return None;
     }
 
-    let dropped_walk = match r.new_revwalk() {
-      Ok(rw) => rw,
-      Err(_) => return None,
+    let id_from_str = match submodule.head_id() {
+      Some(id) => id.as_bytes()[0..4].to_hex(),
+      None => String::from_str("????????"),
     };
-    dropped_walk.set_sorting(git2::SORT_TOPOLOGICAL);
-    dropped_walk.hide(&new_id);
-    dropped_walk.push(&current_id);
+    let id_to_str = match submodule.workdir_id() {
+      Some(id) => id.as_bytes()[0..4].to_hex(),
+      None => String::from_str("????????"),
+    };
 
-    for oid in dropped_walk.oid_iter() {
-      println!("-{}", oid);
+    let mut title_change_separator = "..";
+    let mut message: Option<String> = None;
+    let mut have_dropped_revs: bool = false;
+
+    if let Ok(r) = git2::Repository::open(&path) {
+      let mut m = String::new();
+
+      m.push_str(&*name);
+      m.push_str(":");
+
+      let added_walk = match r.new_revwalk() {
+        Ok(rw) => rw,
+        Err(_) => return None,
+      };
+      added_walk.set_sorting(git2::SORT_TOPOLOGICAL);
+      let _ = added_walk.hide(&current_id);
+      let _ = added_walk.push(&new_id);
+
+      for oid in added_walk.oid_iter() {
+        m.push('\n');
+        m.push('+');
+        m.push_str(&*oid.as_bytes()[0..4].to_hex());
+        match r.find_commit(oid) {
+          Ok(c) => match c.message() {
+            Some(cm) => match cm.split('\n').nth(0) {
+              Some(ct) => {
+                m.push(' ');
+                m.push_str(ct);
+              },
+              None => (),
+            },
+            None => (),
+          },
+          Err(_) => (),
+        }
+      }
+
+      let dropped_walk = match r.new_revwalk() {
+        Ok(rw) => rw,
+        Err(_) => return None,
+      };
+      dropped_walk.set_sorting(git2::SORT_TOPOLOGICAL);
+      let _ = dropped_walk.hide(&new_id);
+      let _ = dropped_walk.push(&current_id);
+
+      for oid in dropped_walk.oid_iter() {
+        have_dropped_revs = true;
+        m.push('\n');
+        m.push('-');
+        m.push_str(&*oid.as_bytes()[0..4].to_hex());
+        match r.find_commit(oid) {
+          Ok(c) => match c.message() {
+            Some(cm) => match cm.split('\n').nth(0) {
+              Some(ct) => {
+                m.push(' ');
+                m.push_str(ct);
+              },
+              None => (),
+            },
+            None => (),
+          },
+          Err(_) => (),
+        }
+      }
+
+      if have_dropped_revs {
+        title_change_separator = "...";
+      }
+
+      message = Some(m);
     }
 
-    Some(SubmoduleUpdate{ submodule: submodule, title: title })
+    let title = format!("{} ({}{}{})", name.clone(), id_from_str, title_change_separator, id_to_str);
+
+    Some(SubmoduleUpdate{ name: name, title: title, message: message })
   }
-}
 
-impl<'a> std::fmt::Show for SubmoduleUpdate<'a> {
-  fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-    write!(f, "SubmoduleUpdate {{ path: {}, title: {} }}",
-      self.submodule.path().as_str().unwrap(),
-      self.title,
-      )
+  pub fn get_title(&self) -> &collections::string::String {
+    &self.title
+  }
+
+  pub fn get_message(&self) -> Option<collections::string::String> {
+    self.message.clone()
   }
 }
