24c97a71af8da37e965f68d4d5e0b7b80fea0fcb
Scott Talbot <s@chikachow.org>
2019-12-31 23:17:13 +1100

    Make individual commit titles optional

--- src/submodule_commitmsg/submodule_commit.rs
+++ src/submodule_commitmsg/submodule_commit.rs
@@ -3,31 +3,28 @@ use super::util::short_id_for_commit_in_repo;
 #[derive(Debug)]
 pub struct SubmoduleCommit {
     pub id: std::string::String,
-    pub title: std::string::String,
+    pub title: Option<std::string::String>,
 }
 
 impl SubmoduleCommit {
-    pub fn new(id: &str, title: &str) -> SubmoduleCommit {
+    pub fn new(id: &str, title: Option<std::string::String>) -> SubmoduleCommit {
         SubmoduleCommit {
             id: id.to_owned(),
-            title: title.to_owned(),
+            title: title,
         }
     }
+
     pub fn from_repository_oid(
         r: &git2::Repository,
         oid: git2::Oid,
     ) -> Result<SubmoduleCommit, git2::Error> {
         let id = short_id_for_commit_in_repo(r, oid)?;
+        let title = r
+            .find_commit(oid)
+            .ok()
+            .and_then({ |c| c.message().map(|cm| cm.to_owned()) })
+            .and_then({ |cm| cm.split('\n').nth(0).map(|ct| ct.to_owned()) });
 
-        let title = match r.find_commit(oid)?.message() {
-            Some(cm) => match cm.split('\n').nth(0) {
-                Some(ct) => ct,
-                None => "",
-            }
-            .to_owned(),
-            None => return Err(git2::Error::from_str("")),
-        };
-
-        Ok(SubmoduleCommit::new(&id, &title))
+        Ok(SubmoduleCommit::new(&id, title))
     }
 }
