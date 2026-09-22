6208fe828dd5a9a653515697e1736cfcd5b1362e
Scott Talbot <s@chikachow.org>
2026-09-22 10:46:02 +1000

    fix: adapt optional strings to git2 result APIs

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn main() {
             let path = submodule.path();
             let name = path
                 .to_str()
-                .or_else(|| submodule.name())
+                .or_else(|| submodule.name().ok())
                 .unwrap_or("")
                 .to_owned();
             if !filenames.is_empty() && !filenames.contains(&name) {
